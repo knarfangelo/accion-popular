8898e5e2d0f09102bfbb074f74054256dda812b0
knarfangelo <knarf2003angelo@gmail.com>
2024-08-23 16:48:20 -0500

    Part 1

--- src/app/components/noticias/noticias.component.ts
+++ src/app/components/noticias/noticias.component.ts
@@ -1,28 +1,44 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, Renderer2, signal, ViewChild, ViewEncapsulation } from '@angular/core';
 import { INoticias } from './DB/INoticias';
 import { noticiasJSON } from './DB/noticiasJSON';
-
+import { register, SwiperContainer } from 'swiper/element';
+import Swiper from 'swiper';
+import { SwiperOptions } from 'swiper/types';
+register()
 @Component({
   selector: 'app-noticias',
   standalone: true,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [
     CommonModule,
   ],
   template: ` 
     <header>
-    <h1>Últimas Noticias de Acción Popular</h1>
-      <main>
-        @for (item of items; track $index) {
-          <article>
-            <img [src]="item.img" alt="">
-            <p>{{item.date}}</p>
-            <h3>{{item.title}}</h3>
-            <p>{{item.description}}</p>
-          </article>
-        }
-      </main>
-    </header>
+  <h1>Últimas Noticias de Acción Popular</h1>
+  <main>
+    @for (item of items; track $index) {
+    <article>
+      <img [src]="item.img" alt="">
+      <p>{{item.date}}</p>
+      <h3>{{item.title}}</h3>
+      <p>{{item.description}}</p>
+    </article>
+    }
+  </main>
+  <swiper-container init="false" class="mySwiper swiper-noticias" pagination="true">
+    @for (noticia of items; track $index) {
+    <swiper-slide>
+      <article>
+        <img [src]="noticia.img" alt="">
+        <p>{{noticia.date}}</p>
+        <h3>{{noticia.title}}</h3>
+        <p>{{noticia.description}}</p>
+      </article>
+    </swiper-slide>}
+  </swiper-container>    
+</header>
+
   `,
   styleUrl: './noticias.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -30,5 +46,43 @@ import { noticiasJSON } from './DB/noticiasJSON';
 export class NoticiasComponent { 
 
   items: INoticias[] = noticiasJSON;
+  swiperElements = signal<SwiperContainer | null>(null);
+  swiperInstance: Swiper | null = null;
+
+  updateInfo(index: number) {
+    // Esperar a que swiperInstance esté inicializado
+    if (this.swiperInstance) {
+      this.swiperInstance.slideTo(index);
+    }
+  }
+
+  ngOnInit(): void {
+    const swiperElemConstructor = document.querySelector('.swiper-noticias') as SwiperContainer;
+    const swiperOptions: SwiperOptions = {
+      navigation: {
+        enabled: true,
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      fadeEffect: {
+        crossFade: true
+      },
+      grid: {
+        rows: 2,
+        fill: 'row',
+      },
+      spaceBetween:20,
+      slidesPerView: 1,
+      speed: 1000,
+      effect: 'slide'
+    };
+    
+    Object.assign(swiperElemConstructor!, swiperOptions);
+    this.swiperElements.set(swiperElemConstructor as SwiperContainer);
+    this.swiperElements()?.initialize();
+
+    // Obtener la instancia de Swiper después de inicializarla
+    this.swiperInstance = swiperElemConstructor?.swiper as Swiper;
+  }
 
 }
--- src/app/components/pilares/pilares.component.ts
+++ src/app/components/pilares/pilares.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { register } from 'swiper/element/bundle';
+import { ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA, signal, ViewEncapsulation } from '@angular/core';
+import Swiper from 'swiper';
+import { register, SwiperContainer } from 'swiper/element/bundle';
+import { SwiperOptions } from 'swiper/types';
 register();
 @Component({
   selector: 'app-pilares',
@@ -45,7 +47,8 @@ register();
         <p>Lorem ipsum dolor sit amet consectetur. Tristique malesuada bibendum proin placerat proin faucibus et.</p>
       </div>
     </main>
-    <swiper-container init=false class="mySwiper">
+          <!-- Swiper Container -->
+          <swiper-container init=false class="mySwiper swiper-pilares">
         <swiper-slide *ngFor="let pilar of pilares">
           <div class="pilar">
             <h1>{{ pilar.numero }}</h1>
@@ -54,19 +57,50 @@ register();
           </div>
         </swiper-slide>
       </swiper-container>
+      
   </header>
   `,
   styleUrl: './pilares.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PilaresComponent { 
+  swiperElements = signal<SwiperContainer | null>(null);
+  swiperInstance: Swiper | null = null;
   pilares = [
     { numero: '01', titulo: 'TITULO DEL PILAR', descripcion: 'Lorem ipsum dolor sit amet consectetur. Tristique malesuada bibendum proin placerat proin faucibus et.' },
-    { numero: '02', titulo: 'TITULO DEL PILAR', descripcion: 'Lorem ipsum dolor sit amet consectetur. Tristique malesuada bibendum proin placerat proin faucibus et.' },
-    { numero: '03', titulo: 'TITULO DEL PILAR', descripcion: 'Lorem ipsum dolor sit amet consectetur. Tristique malesuada bibendum proin placerat proin faucibus et.' },
     { numero: '04', titulo: 'TITULO DEL PILAR', descripcion: 'Lorem ipsum dolor sit amet consectetur. Tristique malesuada bibendum proin placerat proin faucibus et.' },
+    { numero: '02', titulo: 'TITULO DEL PILAR', descripcion: 'Lorem ipsum dolor sit amet consectetur. Tristique malesuada bibendum proin placerat proin faucibus et.' },
     { numero: '05', titulo: 'TITULO DEL PILAR', descripcion: 'Lorem ipsum dolor sit amet consectetur. Tristique malesuada bibendum proin placerat proin faucibus et.' },
+    { numero: '03', titulo: 'TITULO DEL PILAR', descripcion: 'Lorem ipsum dolor sit amet consectetur. Tristique malesuada bibendum proin placerat proin faucibus et.' },
     { numero: '06', titulo: 'TITULO DEL PILAR', descripcion: 'Lorem ipsum dolor sit amet consectetur. Tristique malesuada bibendum proin placerat proin faucibus et.' },
   ];
+
+
+  ngOnInit(): void {
+    const swiperElemConstructor = document.querySelector('.swiper-pilares') as SwiperContainer;
+    const swiperOptions: SwiperOptions = {
+      navigation: {
+        enabled: true,
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination:true,
+      effect: 'slide',
+      slidesPerView: 1,
+      spaceBetween:20,
+      grid: {
+        rows: 3,
+        fill: 'row',
+      },
+      speed: 1000,
+    };
+
+    Object.assign(swiperElemConstructor!, swiperOptions);
+    this.swiperElements.set(swiperElemConstructor as SwiperContainer);
+    this.swiperElements()?.initialize();
+
+    // Obtener la instancia de Swiper después de inicializarla
+    this.swiperInstance = swiperElemConstructor?.swiper as Swiper;
+  }
 
 }
