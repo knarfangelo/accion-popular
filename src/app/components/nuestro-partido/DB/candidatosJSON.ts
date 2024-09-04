4e3e6a534459f719abda745b430f0936d35242c5
knarfangelo <knarf2003angelo@gmail.com>
2024-09-04 14:58:48 -0500

    terminando la descripcion de los candidatos

--- src/app/components/nuestro-partido/DB/candidatosJSON.ts
+++ src/app/components/nuestro-partido/DB/candidatosJSON.ts
@@ -17,20 +17,20 @@ export const candidatosJSON:ICandidato[] = [
         nombre: "Juan José",
         apellido: "Abad Cabrera",
         cargo: "Secretario General",
-        descripcion: "Nacido en Callo el 22 de julio de 1981 es un político y abogado peruano.",
+        descripcion: "Nacido en Huancavelica el 23 de enero de 1956. Es un político peruano.",
         foto: "/candidatos/foto2.png",
         propuestas: [],
-        descripcion2: "Fue el alcalde distrital de San Martín de Porres para el periodo 2019-2022."
+        descripcion2: "Fue diputado por Huancavelica durante el periodo 1990-1992 y actualmente es el secretario general del partido Acción Popular, desde el 2023 - 2025."
     },
     {
         id: 2,
         nombre: "María Eugenia",
         apellido: "Nieva Muzurrieta",
         cargo: "Vicepresidente",
-        descripcion: "Nacido en Callo el 22 de julio de 1981 es un político y abogado peruano.",
+        descripcion: "Nacida en la ciudad de Huancayo, Departamento de Junín.",
         foto: "/candidatos/foto3.png",
         propuestas: [],
-        descripcion2: "Fue el alcalde distrital de San Martín de Porres para el periodo 2019-2022."
+        descripcion2: "Laboró en el Congreso de la República, como personal de confianza, desempeñándome como Asesora y como Técnica."
     },
     {
         id: 3,
