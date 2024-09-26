aae74994b00f9fba6e9f9cc4eda5c0f0c281e6bd
knarfangelo <knarf2003angelo@gmail.com>
2024-09-26 10:44:54 -0500

    corrigiendo en sliders

--- src/app/components/nuestro-partido/DB/candidatosJSON.ts
+++ src/app/components/nuestro-partido/DB/candidatosJSON.ts
@@ -32,24 +32,4 @@ export const candidatosJSON:ICandidato[] = [
         propuestas: [],
         descripcion2: "Laboró en el Congreso de la República, como personal de confianza, desempeñándome como Asesora y como Técnica."
     },
-    {
-        id: 3,
-        nombre: "Julio Abraham 4",
-        apellido: "Chávez Chiong",
-        cargo: "Presidente",
-        descripcion: "Nacido en Callo el 22 de julio de 1981 es un político y abogado peruano.",
-        foto: "/candidatos/foto.png",
-        propuestas: [],
-        descripcion2: "Fue el alcalde distrital de San Martín de Porres para el periodo 2019-2022."
-    },
-    {
-        id: 4,
-        nombre: "Julio Abraham 5",
-        apellido: "Chávez Chiong",
-        cargo: "Presidente",
-        descripcion: "Nacido en Callo el 22 de julio de 1981 es un político y abogado peruano.",
-        foto: "/candidatos/foto.png",
-        propuestas: [],
-        descripcion2: "Fue el alcalde distrital de San Martín de Porres para el periodo 2019-2022."
-    },
 ]
